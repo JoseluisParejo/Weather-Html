f292c0a8c96fbd7ce8b8ad2524bae112163f3194
JoseluisParejo <joseluisparejofernandez@gmail.com>
2014-02-14 10:29:37 +0100

    modificacion
    Modificacion por fallo en linea 41

--- weather2.py
+++ weather2.py
@@ -7,32 +7,32 @@ from jinja2 import Template
 def direcviento(direccion):
 #Ahora calcularemos la direccion del viento por grado
          for grado in str(direccion):
-         	if direccion >= 337.5 and direccion < 22.5:
-          return "N"
-          elif direccion >= 22.5 and direccion < 67.5:
-          return "NE"
-          elif direccion >= 67.5 and direccion < 112.5:
-          return "E"
-          elif direccion >= 112.5 and direccion < 157.5:
-          return "SE"
-          elif direccion >= 157.5 and direccion < 202.5:
-          return "S"
-          elif direccion >= 202.5 and direccion < 247.5:
-          return "SO"
-          elif direccion >= 247.5 and direccion < 292.5:
-          return "O"
-          elif direccion >= 292.5 and direccion < 337.5:
-          return "NO"
+                  if direccion >= 337.5 and direccion < 22.5:
+                           return "N"
+                  elif direccion >= 22.5 and direccion < 67.5:
+                           return "NE"
+                  elif direccion >= 67.5 and direccion < 112.5:
+                           return "E"
+                  elif direccion >= 112.5 and direccion < 157.5:
+                           return "SE"
+                  elif direccion >= 157.5 and direccion < 202.5:
+                           return "S"
+                  elif direccion >= 202.5 and direccion < 247.5:
+                           return "SO"
+                  elif direccion >= 247.5 and direccion < 292.5:
+                           return "O"
+                  elif direccion >= 292.5 and direccion < 337.5:
+                           return "NO"
           
-ciudades = 
- ["Almería",
-  "Cádiz",
-  "Córdoba",
+ciudades = """
+  "Almeria",
+  "Cadiz",
+  "Cordoba",
   "Granada",
   "Huelva",
-  "Jaén",
-  "Málaga",
-  "Sevilla"]
+  "Jaen",
+  "Malaga",
+  "Sevilla" """
   
   
 respuesta = requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':'%s,spain' % ciudades[1]})
